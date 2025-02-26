9003753a12b1f6057155b7fc49933d081d6ff20a
Danis Yangaliev <dyangali@asu.edu>
2025-02-26 07:31:36 -0700

    preppping

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ import numpy as np
 
 extensions = [
     Extension(
-        "cgtools._actual_math.mycmath",           # Full module name
-        ["cgtools/_actual_math/mycmath.pyx"],     # Path to your .pyx file
+        "reforge._actual_math.mycmath",           # Full module name
+        ["reforge/_actual_math/mycmath.pyx"],     # Path to your .pyx file
         include_dirs=[np.get_include()],     # Include numpy headers if needed
         extra_compile_args=["-O3"],
     )
 ]
 
 setup(
-    name="cgtools",
+    name="reforge",
     version="0.1.0",
     packages=find_packages(),
     ext_modules=cythonize(extensions),
