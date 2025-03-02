aa4c7bf6e5c2758cb12c6ec4aba96edeeb5c31fa
Danis Yangaliev <dyangali@asu.edu>
2025-03-02 09:57:39 -0700

    example

--- docs/examples/go_protein.py
+++ docs/examples/go_protein.py
@@ -19,24 +19,30 @@ from reforge import cli, io, mdm
 from reforge.mdsystem.gmxmd import GmxSystem, GmxRun
 from reforge.utils import *  # Assuming this imports required utilities
 
+cli.run('rm -rf tests/test/*')
 #%%
-# Preparing the files
-mdsys = GmxSystem('tests/test', 'test')
+# First, we need to initialize an instance of GmxSystem which will take care of pathing
+# and necessary files. This will use parent directory 'tests' relative to the current directory
+# and root directory 'test' in 'tests' for our system
+mdsys = GmxSystem(sysdir='tests', sysname='test')
+#%%
+# This command will actually prepare the necessary files
 mdsys.prepare_files()
-mdsys.sort_input_pdb("tests/1btl.pdb")
+#%%
+# This will sort chains and atoms in our PDB to avoid conflicts in the future and make file 'inpdb.pdb',
+# which we can access later as mdsys.inpdb
+mdsys.sort_input_pdb("../1btl.pdb")
+print(mdsys.inpdb)
 
 #%%
-# # 2. Clean the PDB and split chains using GROMACS.
-# mdsys.clean_pdb_gmx(in_pdb=mdsys.inpdb, clinput="8\n 7\n", ignh="no", renum="yes")
-# mdsys.split_chains()
-# mdsys.clean_chains_gmx(clinput="8\n 7\n", ignh="yes", renum="yes")
-# mdsys.get_go_maps(append=True)
+# Even though we don't need to have multiple chains in this case, this command splits our mdsys.inpdb
+# into separate chains and moves Proteins and RNA/DNA to their respective directories
+mdsys.split_chains()
 
-# # 3. Coarse-grain the proteins and RNA.
-# mdsys.martinize_proteins_go(go_eps=10.0, go_low=0.3, go_up=1.0, p="backbone", pf=500, append=False)
-# mdsys.martinize_rna(ef=200, el=0.3, eu=1.2, p="backbone", pf=500, append=True)
-# mdsys.make_martini_topology_file(add_resolved_ions=False, prefix="chain")
-# mdsys.make_cgpdb_file(bt="dodecahedron", d="1.2")
+#%%
+# Coarse-grain the proteins using martinize2 by Martini
+mdsys.martinize_proteins_go(go_eps=10.0, go_low=0.3, go_up=1.0, p="backbone", pf=500, append=False)
+# Let's take a look at generated files:
 
 # # 4. Solvate and add ions.
 # solvent = os.path.join(mdsys.wdir, "water.gro")
